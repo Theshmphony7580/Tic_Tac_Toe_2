bca4cbd44ff14540910cd10af63d622c7ce7376c
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2026-04-11 12:49:44 +0530

    made the supabase connection

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
+  const router = useRouter();
+  const handleSignIn = () => {
+    router.push("/signIn");
+  }
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>
+        This is HomePage
+      </h1>
+      <button onClick={handleSignIn}>
+        login
+      </button>
+
     </div>
   );
 }
